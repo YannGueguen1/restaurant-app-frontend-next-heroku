a015866f894d581ecb2ee6ad1e38f13be1824641
YannGueguen1 <yanngueguen2015@u.northwestern.edu>
2022-04-14 21:10:14 -0500

    modified package.json

--- components/restaurantList.js
+++ components/restaurantList.js
@@ -55,9 +55,11 @@ if(searchQuery.length > 0){
         <CardImg
           top={true}
           style={{ height: 200 }}
-          src={
-          `http://localhost:1337`+ res.image.url
-          }
+          // src={
+          // `http://localhost:1337`+ res.image.url
+          // }
+          src={ process.env.NODE_ENV === "production"
+          ? res.image.url : `${process.env.NEXT_PUBLIC_API_URL}${res.image.url}`}
         />
         <CardBody>
           <CardText>{res.description}</CardText>
